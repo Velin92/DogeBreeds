c77027c0f0c3e175a3af1b647a54cda93bf3d389
Mauro Romito <maurovelin@yahoo.com>
2022-02-13 17:55:37 +0100

    Initial Commit

---
+++ DogeBreeds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DogeBreeds
+//
+//  Created by Mauro Romito on 13/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DogeBreeds/DogeBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogeBreedsApp.swift
+//  DogeBreeds
+//
+//  Created by Mauro Romito on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct DogeBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
